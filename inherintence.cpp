93b65ab36f30ed5fb75b9450190d4329fce584cd
wiratamasultan65-max <wiratamasultan65@gmail.com>
2026-05-12 15:59:59 +0700

    pembuatan class orang sebagai base class (parent)

--- inherintence.cpp
+++ inherintence.cpp
@@ -1,5 +1,14 @@
-#ifndef JANTUNG_H
-#define JANTUNG_H
-
 #include <iostream>
+#include <string>
 using namespace std;
+
+// pembuatan class orang sebagai base class (parent)
+class orang {
+public:
+    // deklarasi member variabel dari class orang
+    string nama;
+
+    // pembuatan constructor dan destructor dari class orang
+    orang(string pNama) : nama(pNama) {
+        cout << "Orang dibuat\n" << endl;
+    }
