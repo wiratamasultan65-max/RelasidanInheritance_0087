1d8b813c66cd8ab049a89e2467de2fcf8da1ffd5
wiratamasultan65-max <wiratamasultan65@gmail.com>
2026-05-12 15:53:44 +0700

    pembuatan class dokter

--- asosiasi.cpp
+++ asosiasi.cpp
@@ -25,3 +25,10 @@ public:
     void tambahDokter(dokter*);
     void cetakDokter();
 };
+
+// pembuatan class dokter
+class dokter {
+public:
+    // deklarasi member variabel dari class dokter
+    string nama;
+    vector<pasien*> daftar_pasien;
