0067c76313ccc1c2beaf53ed4173b94338719e5b
wiratamasultan65-max <wiratamasultan65@gmail.com>
2026-05-12 15:52:09 +0700

    deklarasi member variabel dari class pasien dan pembuatan constructor dan destructor dari class pasien

--- asosiasi.cpp
+++ asosiasi.cpp
@@ -5,3 +5,15 @@ using namespace std;
 
 // deklarasi class dokter
 class dokter;
+
+// deklarasi dan pembuatan class pasien
+class pasien {
+public:
+    // deklarasi member variabel dari class pasien
+    string nama;
+    vector<dokter*> daftar_dokter;
+
+    // pembuatan constructor dan destructor dari class pasien
+    pasien(string pNama) : nama(pNama) {
+        cout << "Pasien \"" << nama << "\" ada\n";
+    }
