588cefbddf340a70144ead80841407863e7769a4
wiratamasultan65-max <wiratamasultan65@gmail.com>
2026-05-12 16:01:43 +0700

    pembuatan function perkenalan

--- inherintence.cpp
+++ inherintence.cpp
@@ -33,3 +33,13 @@ public:
     pelajar(string pNama, string pSekolah) : orang(pNama), sekolah(pSekolah) {
         cout << "Pelajar dibuat\n" << endl;
     }
+
+     ~pelajar() {
+        cout << "Pelajar dihapus\n" << endl;
+    }
+
+    // pembuatan function perkenalan()
+    string perkenalan() {
+        return "Hallo, nama saya " + nama + " dari sekolah " + sekolah + "\n\n";
+    }
+};
